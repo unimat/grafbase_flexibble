ee8c92e8ed7273173ff1c59b02af796d2b22cad4
unimat <unimat@unimat.hu>
2023-09-16 09:59:26 +0200

    modify next.config

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -18,27 +18,27 @@ type Provider = {
 
 
 const AuthProviders = () => {
-    const [providers, setProviders] = useState<Providers | null>(null);
+  const [providers, setProviders] = useState<Providers | null>(null);
 
-    useEffect(() => {
-        const fetchProviders = async () => {
-            const res = await getProviders();
-    
-            setProviders(res);
-        }
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
 
-        fetchProviders();
-    }, []);
-
-    if (providers) {
-        return (
-            <div>
-                {Object.values(providers).map((provider: Provider, i) => (
-                    <Button key={i} title='Sign In' handleClick={() => signIn(provider?.id)} />
-                ))}
-            </div>
-        )
+      setProviders(res);
     }
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+            <Button key={i} title='Sign In' handleClick={() => signIn(provider?.id)} />
+        ))}
+      </div>
+    )
+  }
 }
 
 export default AuthProviders
