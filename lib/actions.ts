6e4c7b79aef9c1d8a1777e0e042090bc1aa781df
unimat <unimat@unimat.hu>
2023-09-16 10:59:16 +0200

    Set IsProduction to true in code

--- lib/actions.ts
+++ lib/actions.ts
@@ -13,12 +13,15 @@ import {
 
 import { ProjectForm } from "@/common.types";
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production'; // Always is false ???!!!
+const isProduction = true;
 const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
 const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
 
 const client = new GraphQLClient(apiUrl);
+
+console.log(isProduction);
 
 export const fetchToken = async () => {
   try {
